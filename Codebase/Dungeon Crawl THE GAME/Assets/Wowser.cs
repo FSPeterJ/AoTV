3026ed15ff1b4074cf3c8c7b29535adc6a602988
Ryan Schoener <RSchoener@fullsail.edu>
2016-11-22 16:42:08 -0500

    Started coding for bowser character.

---
+++ Codebase/Dungeon Crawl THE GAME/Assets/Wowser.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wowser : MonoBehaviour
+{
+    int WowX;
+    int WowY;
+    int tail;
+    bool IsIdle = true;
+    bool IsStunned = false;
+	
+
+    public void Follow()
+    {
+        while(IsStunned == false)
+        {
+
+        }
+    }
+}
