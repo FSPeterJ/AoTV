a1148c367677e2df3d3d3634fc54f00421be65de
Klypto <Klypto@KlyptoTech.com>
2016-11-22 16:20:25 -0500

    Added Player Script BasePlayer

---
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/BasePlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("up") || Input.GetKey("w"))
+        {
+            //transform.Translate()
+        }
+
+	}
+}
