92ad5df527164330770e2c14919eafaefe333f9d
Klypto <Klypto@KlyptoTech.com>
2016-11-28 23:05:14 -0500

    Added Jump Added WASD movement Changed Arena platform to mesh collider Connected camera to player body movement
    ToDo:
    Change Strafe to Rotate 90
    Adjust Camera position
    Adjust Jump power

--- Codebase/Dungeon Crawl THE GAME/Assets/Scripts/BasePlayer.cs
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/BasePlayer.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
+
 public class BasePlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey("up") || Input.GetKey("w"))
-        {
-            transform.Translate(new Vector3(0,0,1));
-        }
 
-	}
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Update is called once per frame
+    void Update () {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+
+            if (Input.GetKey("space"))
+
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
 }
