ade0c60399f3a9c910e9a2acc56106c2084552c6
Harrison Jewett <dragor1919@gmail.com>
2016-11-29 10:46:09 -0500

    Smushed all the code together. Rotating mario works, mario can sprint, mario can double jump.

--- Codebase/Dungeon Crawl THE GAME/Assets/Scripts/BasePlayer.cs
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/BasePlayer.cs
@@ -17,6 +17,7 @@ public class BasePlayer : MonoBehaviour
     public float sprintSpeed = 10.0f;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
+    public float rotationSpeed = 2.0f;
     private Vector3 moveDirection = Vector3.zero;
     public int numberOfJumps = 3;
     private int currentJump = 3;
@@ -31,7 +32,7 @@ public class BasePlayer : MonoBehaviour
         if (controller.isGrounded)
         {
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             if (Input.GetKey(KeyCode.LeftShift))
                 moveDirection *= sprintSpeed;
@@ -48,13 +49,15 @@ public class BasePlayer : MonoBehaviour
             moveDirection.y = jumpSpeed;
         }
 
-        if (Input.GetKeyDown("E") && canGrabTail)
+        if (Input.GetKeyDown(KeyCode.E) && canGrabTail)
         {
             
         }
 
 
         moveDirection.y -= gravity * Time.deltaTime;
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed, 0);
+
         controller.Move(moveDirection * Time.deltaTime);
     }
     void OnCollisionEnter (Collision col)
