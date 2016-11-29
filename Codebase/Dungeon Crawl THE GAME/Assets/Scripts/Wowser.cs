d41178495b9b259e6391578b904663e37caae014
Ryan Schoener <RSchoener@fullsail.edu>
2016-11-29 09:59:22 -0500

    Made bowser go after mario if they are not in the same spot.

---
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/Wowser.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wowser : MonoBehaviour
+{
+    public float WowX;
+    public float WowY;
+    public float MarX;
+    public float MarY;
+    public int tail;
+    public bool IsIdle = true;
+    public bool IsStunned = false;
+
+    public void Start()
+    {
+        WowX = 4.0f;
+        WowY = 5.0f;
+        MarX = 10.0f;
+        MarY = 15.0f;
+        IsIdle = true;
+        IsStunned = false;
+    }
+	
+
+    public void Follow()
+    {
+        if(IsStunned == false)
+        {
+            while(WowX != MarX || WowY != MArY)
+            {
+                if (MarX > WowX)
+                    ++WowX;
+                else if (MarX < WowX)
+                    --WowX;
+                if (MarY > WowY)
+                    ++WowY;
+                else if (MarY < WowY)
+                    --WowY;
+            }
+        }
+    }
+}
