67536855dcbc4eacfa92a20593127887a758be2a
Harrison Jewett <dragor1919@gmail.com>
2016-11-29 10:25:22 -0500

    Added the start of mario grabbing wowser's tail

--- Codebase/Dungeon Crawl THE GAME/Assets/Scripts/BasePlayer.cs
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/BasePlayer.cs
@@ -4,31 +4,41 @@ using System.Collections;
 
 
 
-public class BasePlayer : MonoBehaviour {
+public class BasePlayer : MonoBehaviour
+{
 
     // Use this for initialization
-    void Start () {
-	
-	}
+    void Start()
+    {
+
+    }
 
     public float speed = 6.0F;
+    public float sprintSpeed = 10.0f;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
     public int numberOfJumps = 3;
     private int currentJump = 3;
+    private bool canGrabTail = false;
+
+    public GameObject getTail;
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
         {
 
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
+            if (Input.GetKey(KeyCode.LeftShift))
+                moveDirection *= sprintSpeed;
+            else
+                moveDirection *= speed;
 
-            currentJump = numberOfJumps;            
+            currentJump = numberOfJumps;
 
         }
 
@@ -36,10 +46,25 @@ public class BasePlayer : MonoBehaviour {
         {
             currentJump--;
             moveDirection.y = jumpSpeed;
+        }
+
+        if (Input.GetKeyDown("E") && canGrabTail)
+        {
             
         }
 
+
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+    }
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject == getTail)
+            canGrabTail = true;
+    }
+    void OnCollisionExit (Collision col)
+    {
+        if (col.gameObject == getTail)
+            canGrabTail = false;
     }
 }
