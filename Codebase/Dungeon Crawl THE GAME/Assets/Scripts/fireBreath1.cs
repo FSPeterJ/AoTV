8855d54ef39ddaf265b2c99acdf628e9b9b0e5a0
Shane Collins <SPCollins@fullsail.edu>
2016-12-01 16:17:57 -0500

    fix last commt

---
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/fireBreath1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireBreath : MonoBehaviour
+{
+    public GameObject Wowser;
+
+
+    void OnCollisionEnter(Collider col)
+    {
+        if (col.tag =="Player")
+        {
+            if (Wowser.GetComponent<Wowser>().CurrentState == BossStates.Moving)
+                Wowser.GetComponent<Wowser>().CurrentState = BossStates.FireBreath;
+        }
+    }
+}
