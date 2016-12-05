d2d1c135495cf25b9a4ecca7b1184d1383990023
Shane Collins <SPCollins@fullsail.edu>
2016-12-05 17:04:30 -0500

    Continued work on ChargeState

--- Codebase/Dungeon Crawl THE GAME/Assets/Scripts/Wowser.cs
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/Wowser.cs
@@ -21,7 +21,7 @@ public class Wowser : MonoBehaviour
     int bHealth = 3;
     public bool isFireBreath = false;
     public bool PlayerInRange = false;
-    
+
     bool ResetTime;
     public BossStates CurrentState = BossStates.Idle;
     public GameObject Mario;
@@ -83,32 +83,28 @@ public class Wowser : MonoBehaviour
     //
     private void ChargeState()
     {
+        StartCoroutine(ChargeAttack(1));
+   //    if (hasoccured == true)
+   //    {
+   //        Nav.enabled = false;
+   //        GetComponentInChildren<Rigidbody>().isKinematic = false;
+   //        distance = Vector3.Distance(transform.position, MarioPos);
+   //        MarioPos = Mario.transform.position;
+   //        hasoccured = false;
+   //        Rigid.useGravity = true;
+   //        starttime = Time.time;
+   //    }
+   //
+   //    Vector3 heading = MarioPos - transform.position;
+   //    Vector3 startlocation = transform.forward;
+   //    Rigid.AddForce(transform.forward, ForceMode.VelocityChange);
+   //    if (transform.position == heading)
+   //    {
+   //        CurrentState = BossStates.Moving;
+   //    }
+        // to break we need to check collision with mario or a set distance
 
-        if (hasoccured == true)
-        {
-            Nav.enabled = false;
-            GetComponentInChildren<Rigidbody>().isKinematic = false;
-            distance = Vector3.Distance(transform.position, MarioPos);
-            MarioPos = Mario.transform.position;
-            hasoccured = false;
-            Rigid.useGravity = true;
-             starttime= Time.time;
-        }
-        //   Vector3.Distance(transform.position, MarioPos)
-        //   float step = 10 * Time.deltaTime;
-        Vector3 heading = MarioPos - transform.position;
-        Rigid.AddForce(transform.forward, ForceMode.VelocityChange);
-      //  transform.forward
-      //  Vector3.MoveTowards(transform.position, MarioPos,30);
-        if (transform.position==MarioPos)
-        {
-            CurrentState = BossStates.Moving;
-        }
 
-      //  float dist = (Time.time - starttime) * 10;
-      //  float frac = dist / distance;
-      //  transform.position = Vector3.Lerp(transform.position, MarioPos, frac);
-       
     }
 
     void IdleState()
@@ -119,19 +115,19 @@ public class Wowser : MonoBehaviour
 
     void MovingState()
     {
-        
+
 
         if (Nav.remainingDistance < 4f)
         {
 
             Nav.speed = 0.5f;
         }
-        else if (Nav.remainingDistance >= 5f&& Nav.remainingDistance<11f)
+        else if (Nav.remainingDistance >= 5f && Nav.remainingDistance < 11f)
         {
             Nav.speed = 3;
 
         }
-        else if (Nav.remainingDistance>10f)
+        else if (Nav.remainingDistance > 10f)
         {
             CurrentState = BossStates.Charge;
         }
@@ -204,7 +200,7 @@ public class Wowser : MonoBehaviour
 
         GetComponentInChildren<TriggerFireEvent>().DisableParticleSystem();
         isFireBreath = false;
-   
+
         yield return new WaitForSeconds(1.5f);
         CurrentState = BossStates.Moving;
 
@@ -220,11 +216,11 @@ public class Wowser : MonoBehaviour
         //TimeUntil Stomp Starts
         //yield return new WaitForSeconds(seconds);
 
-        
+
         Nav.Stop(true);
-        
+
         Nav.enabled = false;
-        
+
         Rigid.isKinematic = false;
 
         Rigid.useGravity = true;
@@ -238,7 +234,7 @@ public class Wowser : MonoBehaviour
         Rigid.useGravity = false;
         //Nav.Warp(transform.position);
         Nav.enabled = true;
-        
+
 
         //time until continues to walk
         yield return new WaitForSeconds(1.5f);
@@ -249,11 +245,11 @@ public class Wowser : MonoBehaviour
     }
 
 
-        //From The following:
-        //http://answers.unity3d.com/questions/714835/best-way-to-spawn-prefabs-in-a-circle.html
-        Vector3 RandomCircle(Vector3 center, float radius)
+    //From The following:
+    //http://answers.unity3d.com/questions/714835/best-way-to-spawn-prefabs-in-a-circle.html
+    Vector3 RandomCircle(Vector3 center, float radius)
     {
-        
+
         float ang = UnityEngine.Random.value * 360;
         Vector3 pos;
         pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
@@ -261,6 +257,33 @@ public class Wowser : MonoBehaviour
         pos.z = center.z;
         return pos;
     }
-}
+    IEnumerator ChargeAttack(float seconds)
+    {
+        if (isCoroutineExecuting)
+            yield break;
+        isCoroutineExecuting = true;
+        //TimeUntil Stomp Starts
+        //yield return new WaitForSeconds(seconds);
 
+
+        Nav.Stop(true);
+
+
+        Nav.enabled = false;
+        Rigid.isKinematic = false;
+        Rigid.useGravity = true;
+
+        Vector3 forcedirection = transform.forward * 1.2f;
+        Rigid.AddForce(forcedirection, ForceMode.Impulse);
+        yield return new WaitForSeconds(1);
+       
+        yield return new WaitForSeconds(1);
+        Rigid.isKinematic = true;
+        Rigid.useGravity = false;
+        Nav.enabled = true;
+        yield return new WaitForSeconds(1);
+        CurrentState = BossStates.Moving;
+        isCoroutineExecuting = false;
+    }
+}
 
