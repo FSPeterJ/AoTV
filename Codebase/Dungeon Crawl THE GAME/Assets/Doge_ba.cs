1fdd90943df3019bbb631f31205940dfa447dfc5
Ryan Schoener <RSchoener@fullsail.edu>
2016-12-06 16:06:33 -0500

    Began scripting for doge-ba.

--- Codebase/Dungeon Crawl THE GAME/Assets/Doge_ba.cs
+++ Codebase/Dungeon Crawl THE GAME/Assets/Doge_ba.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Doge_ba : MonoBehaviour {
+public class Doge_ba : MonoBehaviour
+{
+    public float DogeX;
+    public float DogeY;
+    public float DogeSpeed = 5.0f;
+    public float attack;
+    public bool IsAlive = true;
+    public bool AttackMode = false;
+    void Movement()
+    {
+        while (IsAlive)
+        {
+            if (AttackMode)
+            {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+            }
+        }
+    }
 }
