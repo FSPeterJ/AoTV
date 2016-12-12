395f59c7d10da226308c3701f78a2d4fd7371e74
Ryan Schoener <RSchoener@fullsail.edu>
2016-12-12 11:19:31 -0500

    committing to pull

--- Codebase/Dungeon Crawl THE GAME/Assets/Canon1.cs
+++ Codebase/Dungeon Crawl THE GAME/Assets/Canon1.cs
@@ -3,14 +3,24 @@ using System.Collections;
 
 public class Canon1 : MonoBehaviour
 {
+    public Transform ballcannon;
+    public float foceshot;
+    private Transform pointShot;
 
 	// Use this for initialization
 	void Start()
     {
-	    while (true)
-        {
-
-        }
+        pointShot = transform.Find("Mario");
 	}
-	
+
+    float lastime = 0;
+
+    void Update()
+    {
+        if (Time.time - lastime >= 5)
+        {
+            GameObject instancia = (GameObject) Instantiate(ballcannon, pointShot.position, pointShot.rotation);
+            
+        }
+    }
 }
