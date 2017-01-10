500ddbc1a8935415d83d09f749986d210a71454b
Michael Manahan <mikemanahan15@gmail.com>
2017-01-10 17:22:25 -0500

    My Sandbox

--- Codebase/Dungeon Crawl THE GAME/Assets/Scripts/EventSystem.cs
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/EventSystem.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public static class EventSystem {
+public static class EventSystem
+{
     public delegate void PlayerPositionUpdateHandler(Vector3 pLevelToLoad);
     public static event PlayerPositionUpdateHandler onPlayerPositionUpdate;
     public static void PlayerPositionUpdate(Vector3 pLevelToLoad)
