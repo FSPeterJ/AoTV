a114023accdb84bc10305622ee5775b77cdb046a
rjs5785 <rjs5785@hotmail>
2017-01-12 12:17:52 -0500

    Got the timer working. Now just trying to figure out how to get it to start once the player is able to start moving.

---
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/System/Load 'n Save.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LoadnSave : MonoBehaviour
+{
+    //defining the Stream variables
+    StreamReader sr;
+    StreamWriter sw;
+
+    //defining stream path variables
+    string pScore;
+    string currLevel;
+
+	// Use this for initialization
+	void Start()
+    {
+        sr = new StreamReader(pScore);
+        sr = new StreamReader(currLevel);
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+}
---
+++ Codebase/Dungeon Crawl THE GAME/Assets/Scripts/System/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    //defining the variables needed
+    float time = 0;
+    int x = Screen.width / 2;
+    int y = 0;
+
+	// Use this for initialization
+	void Start()
+    {
+        Update();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        time = time += Time.deltaTime;
+    }
+
+    void OnGUI()
+    {
+       GUI.Label(new Rect(x, y, 200f, 200f), time.ToString());
+    }
+}
