6761f79254657ff6712c2e449cb7f53ca60c4927
Shane Collins <SPCollins@fullsail.edu>
2017-01-29 23:10:25 -0500

    Mostly Finished Forest.unity

---
+++ Codebase/Dungeon Crawl THE GAME/Assets/ChestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour {
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("Open", false);
+    }
+    void OnTriggerEnter(Collider Col)
+    {
+        if(Col.tag == "Player")
+        {
+            anim.SetBool("Open", true);
+            //instaniate health object
+        }
+    }
+}
