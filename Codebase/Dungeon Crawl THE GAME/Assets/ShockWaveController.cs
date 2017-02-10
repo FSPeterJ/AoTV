07ccad473904f770fdc606f4f49cc23b9564052f
Shane Collins <SPCollins@fullsail.edu>
2017-02-09 19:29:39 -0500

    ShockWaveController work for treant
    Former-commit-id: 5d5a2527683e5d96631e01e727cd2581a01ffdfe

---
+++ Codebase/Dungeon Crawl THE GAME/Assets/ShockWaveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockWaveController : MonoBehaviour {
+   
+    [SerializeField] bool shockwaveActive = false;
+    CapsuleCollider cCollider;
+    float velosity = 2;
+    float newScale = 0;
+	// Use this for initialization
+	void Start () {
+        cCollider = GetComponent<CapsuleCollider>();
+        cCollider.transform.localScale = new Vector3(newScale, 1, newScale);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(shockwaveActive==true)
+        {
+            if (cCollider.transform.localScale.x <1)
+            {
+                newScale += velosity * Time.deltaTime;
+                cCollider.transform.localScale = new Vector3(newScale, 1, newScale);  
+            }
+        }
+	}
+}
