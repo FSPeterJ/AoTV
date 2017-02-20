1d1ae5e74297018718f54540f93034b5c0d45349
Michael Manahan <mikemanahan15@gmail.com>
2017-02-20 15:28:06 -0500

    Dragon Boss Fight
    Former-commit-id: a5ce36da1abce2eb6bb800ce213dd0b2faf6c083

---
+++ Codebase/Dungeon Crawl THE GAME/Assets/BeginBossBattle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginBossBattle : MonoBehaviour {
+    
+    public Dragon drag;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            drag.SendMessage("StartFight");
+        }
+    }
+}
